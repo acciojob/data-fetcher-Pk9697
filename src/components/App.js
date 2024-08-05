8bb731c2f133a4675423570323ac8eb355c87260
Prashant Kumar <prashant.aman9@gmail.com>
2024-08-05 21:02:13 +0530

    Complete Assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,35 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useEffect, useState } from 'react'
+import './../styles/App.css'
 
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
+	const [status, setStatus] = useState('idle')
+	const [data, setData] = useState('')
+	const [error, setError] = useState(null)
+
+	useEffect(() => {
+		setStatus('loading')
+		fetch('https://dummyjson.com/products')
+			.then((response) => response.json())
+			.then((data) => {
+				setData(data)
+				setStatus('success')
+				setError(null)
+			})
+      .catch((error) => {
+        setData('')
+				setStatus('error')
+				setError(error)
+			})
+	}, [])
+
+	return (
+		<div>
+			{/* Do not remove the main div */}
+			{status === 'loading' && <p>Loading...</p>}
+			{data && <pre>{JSON.stringify(data, null, 2)}</pre>}
+			{status === 'error' && error && <p>Error: {error.message}</p>}
+		</div>
+	)
 }
 
 export default App
